9a5deaedc16b5f89c3210e4616a0aafb38671175
shrutivanw <shruti.dalvi@gmail.com>
2018-02-21 15:12:48 -0800

    C9 updates
    Changed the assignment to leverage minitests

---
+++ lib/binary_to_decimal.rb
@@ -0,0 +1,9 @@
+# A method named `binary_to_decimal` that receives as input an array of size 8.
+# The array is randomly filled with 0’s and 1’s.
+# The most significant bit is at index 0.
+# The least significant bit is at index 7.
+# Calculate  and return the decimal value for this binary number using
+# the algorithm you devised in class.
+def binary_to_decimal(binary_array)
+  raise NotImplementedError
+end

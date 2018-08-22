d0f44c3beebc2aa46f2deface788e816bb057a81
shrutivanw <shruti@adadevelopersacademy.org>
2018-08-22 16:00:26 -0700

    Update binary_to_decimal.rb
    Updated solution

--- lib/binary_to_decimal.rb
+++ lib/binary_to_decimal.rb
@@ -5,5 +5,10 @@
 # Calculate  and return the decimal value for this binary number using
 # the algorithm you devised in class.
 def binary_to_decimal(binary_array)
-  raise NotImplementedError
+  decimal = 0
+  bit_count = binary_array.length 
+  binary_array.length.times do |i|
+    decimal += (2 ** i) * binary_array[bit_count - i - 1]
+  end
+  return decimal
 end

411e12c5803af61ee07eae06f6f887a915822fbd
Chris M <chris@adadevelopersacademy.org>
2019-08-24 13:50:04 -0700

    Add spec reporter

--- test/binary_to_decimal_test.rb
+++ test/binary_to_decimal_test.rb
@@ -2,6 +2,8 @@ require 'minitest/autorun'
 require 'minitest/reporters'
 require_relative '../lib/binary_to_decimal'
 
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+
 describe "binary to decimal" do
   it "From 10011001 to 153" do
     binary_array = [1, 0, 0, 1, 1, 0, 0, 1]

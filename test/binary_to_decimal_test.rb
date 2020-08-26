b7dde471181d69cb5632c340beb634b2a63fb03d
CheezItMan <chris@adadevelopersacademy.org>
2020-08-25 17:20:15 -0700

    move tests to expect syntax

--- test/binary_to_decimal_test.rb
+++ test/binary_to_decimal_test.rb
@@ -9,27 +9,27 @@ describe "binary to decimal" do
     binary_array = [1, 0, 0, 1, 1, 0, 0, 1]
     expected_decimal = 153
 
-    binary_to_decimal(binary_array).must_equal expected_decimal
+    expect(binary_to_decimal(binary_array)).must_equal expected_decimal
   end
 
   it "From 00001101 to 13" do
     binary_array = [0, 0, 0, 0, 1, 1, 0, 1]
     expected_decimal = 13
 
-    binary_to_decimal(binary_array).must_equal expected_decimal
+    expect(binary_to_decimal(binary_array)).must_equal expected_decimal
   end
 
   it "From 10000000 to 128" do
     binary_array = [1, 0, 0, 0, 0, 0, 0, 0]
     expected_decimal = 128
 
-    binary_to_decimal(binary_array).must_equal expected_decimal
+    expect(binary_to_decimal(binary_array)).must_equal expected_decimal
   end
 
   it "From random binary to decimal" do
     binary_array = Array.new(8) { rand(0..1) }
     expected_decimal = binary_array.join.to_s.to_i(2)
 
-    binary_to_decimal(binary_array).must_equal expected_decimal
+    expect(binary_to_decimal(binary_array)).must_equal expected_decimal
   end
 end

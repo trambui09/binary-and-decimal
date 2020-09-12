1c9a7eee32f1a52705c5f6231ad8c42bb8ea3098
Tram Bui <62119967+trambui09@users.noreply.github.com>
2020-09-11 20:31:20 -0500

    Update binary_to_decimal.rb

--- lib/binary_to_decimal.rb
+++ lib/binary_to_decimal.rb
@@ -4,6 +4,25 @@
 # The least significant bit is at index 7.
 # Calculate  and return the decimal value for this binary number using
 # the algorithm you devised in class.
+
+# method to turn binary to decimal
 def binary_to_decimal(binary_array)
-  raise NotImplementedError
+  decimal = 0
+  reversed_binary = binary_array.reverse
+  reversed_binary.each_with_index do |binary, index|
+    decimal += (binary * (2 ** index))
+  end
+  return decimal
 end
+
+# method to turn decimal into an array of binary digits
+def decimal_to_binary(num)
+  binary_array = []
+  until (num.to_f / 2) == 0
+    # prepend will add the new value to the front, instead of the end
+    binary_array.prepend(num % 2)
+    num /= 2
+  end
+  return binary_array
+end
+
